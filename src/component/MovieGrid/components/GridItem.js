0f36161e6c8dc677feb9370955eb40a388d3ec34
Abhijith v <abhijithababhijith@gmail.com>
2022-01-22 19:28:13 +0530

    fix height issue

--- src/component/MovieGrid/components/GridItem.js
+++ src/component/MovieGrid/components/GridItem.js
@@ -21,6 +21,7 @@ const Row = ({ data = [], lastElementRef }) => {
                             /> */}
                             <Image
                                 alt=""
+                                className="md:object-contain md:max-w-[400px] md:max-h-[250px] lg:object-contain lg:max-w-[300px] lg:max-h-[250px]"
                                 src={`images/${item["poster-image"]}`}
                             />
                             <p
@@ -40,6 +41,7 @@ const Row = ({ data = [], lastElementRef }) => {
                                 src={`images/${item["poster-image"]}`} /> */}
                             <Image
                                 alt=""
+                                className="md:object-contain md:max-w-[400px] md:max-h-[250px] lg:object-contain lg:max-w-[300px] lg:max-h-[250px]"
                                 src={`images/${item["poster-image"]}`}
                             />
                             <p
