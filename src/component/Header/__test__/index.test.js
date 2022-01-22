a82321b17835e7307ecc1728e6f98bd67d11d1e8
Abhijith v <abhijithababhijith@gmail.com>
2022-01-22 10:05:36 +0530

    snapshot testing:wip

--- src/component/Header/__test__/index.test.js
+++ src/component/Header/__test__/index.test.js
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import renderer from 'react-test-renderer';
-
 import Header from '../index';
 import { useSelector, useDispatch } from 'react-redux'
+import {configure,shallow} from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
 
 jest.mock('react-redux');
 
@@ -18,6 +21,14 @@ describe('Header testing', () => {
         const tree = renderer.create(<Header />).toJSON();
         expect(tree).toMatchSnapshot();
     })
-    
+    test('children', () => {
+        const tree = renderer.create(<Header />).toJSON();
+        expect(tree.children.length).toBe(3);
+    })
+    test('search element in header',()=>{
+        const wrapper = shallow(<Header />);
+        const input = wrapper.find('input');
+        expect(input.length).toBe(1);
+    })
     
 })
