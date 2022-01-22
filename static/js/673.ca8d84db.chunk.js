57f8e3063052b06217b674b6620c66dcdc874620
Abhijith v <abhi@Abhi-Macbook.local>
2022-01-22 17:55:03 +0530

    Updates

---
+++ static/js/673.ca8d84db.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkOTT_Clone=self.webpackChunkOTT_Clone||[]).push([[673],{673:function(e,s,o){o.r(s);var a=o(413),r=o(152),n=o(791),l=o(184),t=function(e){var s=e.alt,o=void 0===s?"":s,t=e.className,c=void 0===t?"":t,i=e.src,u=void 0===i?"":i,f=e.otherProps,m=void 0===f?{}:f,p=(0,n.useState)(u),d=(0,r.Z)(p,2),v=d[0],h=d[1],k=(0,n.useCallback)((function(){h("images/placeholder_for_missing_posters.png")}),[u,v]);return(0,l.jsx)("img",(0,a.Z)({onError:k,alt:o,className:c,src:v},m))};s.default=(0,n.memo)(t)}}]);
+//# sourceMappingURL=673.ca8d84db.chunk.js.map
